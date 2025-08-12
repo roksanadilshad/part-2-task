74261871af6c3bb8e352891f0df05adcc472fabe
roksanadilshad <tanvirhossaintufa@gmail.com>
2025-08-12 13:30:40 +0600

    average price

---
+++ task4.js
@@ -0,0 +1,20 @@
+const phones = [
+        { model: "PhoneA", brand: "Iphone", price: 95000 },
+        { model: "PhoneB", brand: "Samsung", price: 40000 },
+        { model: "PhoneC", brand: "Oppo", price: 26000 },
+        { model: "PhoneD", brand: "Nokia", price: 35000 },
+        { model: "PhoneE", brand: "Iphone", price: 105000 },
+        { model: "PhoneF", brand: "HTC", price: 48000 },
+    ];
+
+function findAveragePhonePrice(phones){
+    let count = 0;
+    for(let obj of phones){
+      count = (count + obj.price);
+    }
+    return count / phones.length;
+}
+
+    const result = findAveragePhonePrice(phones);
+    console.log(result);
+    
